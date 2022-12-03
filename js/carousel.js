9165d422bb508df5a3f47bdd24efc1af04382aa0
Luis Otavio <luisotavio95@gmail.com>
2022-12-03 20:21:33 -0300

    primeiro commit

---
+++ js/carousel.js
@@ -0,0 +1,21 @@
+var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
+
+// Percorre todos os botoes controladores
+btns.forEach(function(btn) {
+  btn.addEventListener('click', function() {
+    
+
+    // Remove classe 'ativo' dos outros botoes
+    btns.forEach(function(btnRemoveClass) {
+      btnRemoveClass.classList.remove('listaDeArtigos-slider-item--ativo')
+    })
+
+    this.classList.add('listaDeArtigos-slider-item--ativo')
+  })
+})
+
+// formContato = function (event){
+//   var form = event
+//   console.log(form)
+  
+// }
