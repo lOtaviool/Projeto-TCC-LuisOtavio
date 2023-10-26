17deb9e02072a4bb3e81929cc03cf6d091289397
lOtaviool <luisotavio95@gmail.com>
2023-10-26 12:03:30 -0300

    Ajustes layout 5

--- js/dialog-form.js
+++ js/dialog-form.js
@@ -5,11 +5,16 @@ var dialog = document.querySelector('.dialogNewsletter2');
 var dialogBody = document.querySelector('.dialogNewsletter2-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter2-overlay');
 
-
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
-  dialog.classList.add('dialogNewsletter2--aberto');
-  console.log(form)
+  var name = document.querySelector('#name');
+  var email = document.querySelector('#email');
+  var phone = document.querySelector('#phone');
+  var menssage = document.querySelector('#menssage');
+
+  if(name?.value && email.value && phone?.value){
+    dialog.classList.add('dialogNewsletter2--aberto');
+  }
 });
 
 function fechandoDialog() {
